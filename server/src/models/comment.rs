12f057ba6c66250e8a1a3a6e5c8c9d46bc2abc6b
MIracleyin <miracleyin@live.com>
2023-03-20 20:43:49 +0800

    comment struct

--- server/src/models/comment.rs
+++ server/src/models/comment.rs
@@ -4,9 +4,12 @@ use super::user::GithubUserInfo;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Comment {
+    // 评论 ID
     pub id: Option<i32>,
+    // 展示评论用户的 Github 信息
     pub user: Option<GithubUserInfo>,
     pub content: String,
     pub date: Option<chrono::NaiveDate>,
+    // 被评论的文章
     pub article: Option<u32>,
 }
